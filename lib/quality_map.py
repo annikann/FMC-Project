617b73d3a5d938f8dde3d22a204ce63251bf7842
Nathan <144726831+BlimChitz@users.noreply.github.com>
2023-11-12 21:29:55 -0500

    Add files via upload

---
+++ lib/quality_map.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# 50x50 grid of random qualities 1-4
+# 4 = Safe
+# 3 = Viable
+# 2 = Unsafe
+# 1 = Terrible
+
+def quality():
+    size = 50
+    qual = np.zeros([size,size])
+
+    for i in range(size):
+        for j in range(size):
+            qual[i,j] = np.random.randint(1,5)
+
+    return qual, size
+
